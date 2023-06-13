6fd9a132b6c984c70c26733a3bfaecbfee0945e5
Ihar Berasneu <iberesnev@alfabank.ru>
2023-06-13 16:27:58 +0300

    feat: add copy function and constants

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,41 @@
+import fs from 'fs/promises';
+import {constants} from 'fs';
+import {dirname, join} from 'path';
+import { fileURLToPath } from 'url';
+import { ERROR_CODES, ERROR_MESSAGES } from '../constants/errors';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const initFolder = 'files';
+const copyFolder = 'files_copy';
+
+const initFolderPath = join(__dirname, initFolder);
+const copyFolderPath = join(__dirname, copyFolder);
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const isSourceFolderExist =  await fs.access(initFolderPath)
+                                        .then(() => true)
+                                        .catch(() => false);
+        if (!isSourceFolderExist) {
+            throw new Error(ERROR_MESSAGES.fsFailed);
+        }
+
+        await fs.cp(
+            initFolderPath,
+            copyFolderPath,
+            { recursive: true, force: false, errorOnExist: true }                
+        );
+    } catch (error) {
+        if (error.code === ERROR_CODES.cpExist) {
+            throw new Error(ERROR_MESSAGES.fsFailed);
+        }
+       
+        if (error.code !== ERROR_CODES.eoent) {
+            throw error;
+        }  
+    }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,8 +1,7 @@
-
-
 import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { ERROR_CODES, ERROR_MESSAGES } from '../constants/errors';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,9 +11,9 @@ const fileContent = 'I am fresh and young';
 const create = async () => {
   try {
     await fs.access(filePath);
-    throw new Error('File already exists');
+    throw new Error(ERROR_MESSAGES.fsFailed);
   } catch (error) {
-    if (error.code !== 'ENOENT') {
+    if (error.code !== ERROR_CODES.eoent) {
       throw error;
     }
   }  
