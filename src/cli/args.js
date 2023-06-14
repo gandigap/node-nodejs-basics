4f8cc54c4e6c4e066db73497cedf585776f09980
Ihar Berasneu <iberesnev@alfabank.ru>
2023-06-14 20:46:25 +0300

    feat: add functions for article cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    const arguments = process.argv.slice(2); 
+    const result = [];
+
+  for (let index = 0; index < arguments.length; index += 2) {
+    const argumentName = arguments[index].replace('--', '');
+    const argumentValue = arguments[index + 1];
+    result.push(`${argumentName} is ${argumentValue}`);
+  }
+
+  console.log(result.join(', '))
 };
 
 parseArgs();
