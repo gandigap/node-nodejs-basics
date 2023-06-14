f4526a14e90182694db1b9cf7ef44cdec46edea8
Ihar Berasneu <iberesnev@alfabank.ru>
2023-06-14 21:49:12 +0300

    feat: add functions for articles modules and hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { readFile } from 'fs/promises';
+import { createHash } from 'crypto';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { getDirname } from '../utils/get-dirname.js';
+
+const __dirname = getDirname(import.meta.url);
+const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileContent = await readFile(filePath);
+
+    const hash = createHash('sha256');
+    hash.update(fileContent);
+    const hashHex = hash.digest('hex');
+
+    console.log(hashHex);
 };
 
 await calculateHash();
--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,21 +1,31 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp  } from 'http';
+import {fileURLToPath} from "url";
+import { getDirname } from '../utils/get-dirname.js';
+import  './files/c.js';
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = await import('./files/a.json', {
+        assert: { type: 'json' }
+    });
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = await import('./files/b.json', {
+        assert: { type: 'json' }
+    });
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = getDirname(import.meta.url);
 
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
@@ -33,7 +43,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
