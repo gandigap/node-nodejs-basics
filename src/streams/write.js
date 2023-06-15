9c28ccdf3de0b5ee3e21c7f38240aab0b14e1a75
Ihar Berasneu <iberesnev@alfabank.ru>
2023-06-15 16:51:09 +0300

    feat: add functions for streams article

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,28 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
+import { getDirname } from '../utils/get-dirname.js';
+import { pipeline } from "stream/promises";
+
+const __dirname = getDirname(import.meta.url);
+const fileToReadPath = join(__dirname, 'files', 'fileToRead.txt');
+const fileToWritePath = join(__dirname, 'files', 'fileToWrite.txt');
+
+import fs from 'fs';
+
 const write = async () => {
-    // Write your code here 
+    const readStream = createReadStream(fileToReadPath);
+    const writeStream = createWriteStream(fileToWritePath);
+
+    const handleError = () => {
+      console.log('Error');
+      readStream.destroy();
+      writeStream.end('Finished with error...')
+    }
+    
+    readStream
+      .on('error', handleError)
+      .pipe(writeStream)
+      .on('error', handleError);
 };
 
 await write();
