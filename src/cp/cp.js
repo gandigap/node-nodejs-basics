a75dbcb41598901cf2598656d9a286393e2a4a72
Ihar Berasneu <iberesnev@alfabank.ru>
2023-06-19 13:51:00 +0300

    feat: add function for article cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,27 @@
+import { join } from 'path';
+import { fork } from 'child_process';
+import { getDirname } from '../utils/get-dirname.js';
+
+const __dirname = getDirname(import.meta.url);
+const scriptPath = join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = fork(scriptPath, args, { silent: true });
+
+    const handleError = () => {
+        console.log('Error');
+        readStream.destroy();
+        writeStream.end('Finished with error...');
+    }
+
+    process.stdin
+        .on('error', handleError)
+        .pipe(childProcess.stdin);
+
+    childProcess.stdout
+        .on('error', handleError)
+        .pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['someArgument1', 'someArgument2'] );
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,4 +1,8 @@
-// n should be received from main thread
+import {
+    workerData,
+    parentPort
+  } from 'worker_threads';
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
